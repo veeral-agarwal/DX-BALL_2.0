e9249538affa3cbad20ba3dfbddd74f7fdecaaad
veeral-agarwal <veeral.agarwal@research.iiit.ac.in>
2021-03-17 19:42:25 +0530

    collision bullet brick left

--- objects.py
+++ objects.py
@@ -725,8 +725,22 @@ class UFO(Objects):
                         global_variables.b2.append(Brick(config.brick,i+3,9,random.randint(1,3),8))
                 global_variables.main_ball.speed_y*=-1
                 
+    def collision_with_bullet(self):
+        if global_variables.main_bullet.position_y == self.position_y or global_variables.main_bullet.position_y == self.position_y+1:
+            if global_variables.main_bullet.position_x>=self.position_x and global_variables.main_bullet.position_x <=self.position_x+4:
+                self.lives-=1
+                if self.lives == 10 :
+                    for i in range(0,90,3):
+                        global_variables.b1.append(Brick(config.brick,i+3,8,random.randint(1,3),8))
+                if self.lives == 5:
+                    for i in range(0,90,3):
+                        global_variables.b2.append(Brick(config.brick,i+3,9,random.randint(1,3),8))
+                global_variables.main_bullet.speed_y = 0
+                global_variables.main_bullet.position_y = 4
+
     def render(self):
         self.collision_with_ball()
+        self.collision_with_bullet()
         self.position_x = global_variables.main_paddle.position_x
         
         for i in range(self.width):
